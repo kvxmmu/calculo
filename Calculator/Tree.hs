3cc56fa239e2b9c659f491205bbbc5646cb2593e
Nero <resqnetm@gmail.com>
2022-05-15 15:18:38 +0300

    Update tree

--- Calculator/Tree.hs
+++ Calculator/Tree.hs
@@ -1,4 +1,10 @@
 module Calculator.Tree where
     import Calculator.Token (Token)
 
-    data Node a = Plain (Token a) | Tree [Node a]
+    data Node a = Plain (Token a) | Brackets [Node a]
+                                  | Braces [Node a]
+                                  | If { cond  :: [Node a]
+                                       , true  :: [Node a]
+                                       , false :: [Node a] }
+                                  | Let { name :: Token a
+                                        , expr :: [Node a] }
